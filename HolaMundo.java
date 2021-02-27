aa44203915b3f68ed76b45e37589e2d6c6e0962f
P4Hchez <p4hchez@gmail.com>
2021-02-27 10:28:07 +0000

    Definimos colores

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,8 @@
 public class HolaMundo{
 	public static void main(String[] args){
+		String naranja = "\033[33m";
+		String azul = "\033[34m";
+
 		System.out.println("Hello World");
 		System.out.println("Segunda Linea");
 		//Otro comentario
